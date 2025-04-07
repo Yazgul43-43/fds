a0895e3f5251c259a6fb03f04365d7ccc2b2e66a
Admin <yazgul.nigmatullina00@mail.ru>
2025-04-07 14:54:03 +0900

    zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        byte a= 25;
+        System.out.println(a);
+        short b = 270;
+        System.out.println(b);
+        int c = 1000000000;
+        System.out.println(c);
+        long d = 3000000000000000000l;
+        System.out.println(d);
+        float e = 2.75f;
+        System.out.println(e);
+        double f = 3.6;
+        System.out.println(f);
+
+
+
 
     }
-}
+    }
