49dddfefb38e289ce20fe6ceedcde335e1ab4824
Admin <yazgul.nigmatullina00@mail.ru>
2025-04-07 15:27:50 +0900

    zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,20 @@ public class Main {
         double f = 3.6;
         System.out.println(f);
 
-
-
+        double A = 27.12;
+        System.out.println(A);
+        long B = 978678965549l;
+        System.out.println(B);
+        float C = 2.786f;
+        System.out.println(C);
+        short D = 569;
+        System.out.println(D);
+        short E = -159;
+        System.out.println(E);
+        short F = 27897;
+        System.out.println(F);
+        char G = 67;
+        System.out.println(G);
 
     }
     }
